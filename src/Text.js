751fbc6f15e814f41d81d13589f6e4f26d5b6ccd
SeanMee86 <seanmee8186@gmail.com>
2022-03-15 11:22:15 -0700

    add another fail

---
+++ src/Text.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TextComponent = () => {
+    return (
+        <div>
+            <h3>hello</h3>
+            <h3>goodbye</h3>
+        </div>
+    );
+};
+
+export default TextComponent;
